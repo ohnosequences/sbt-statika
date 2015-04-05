bf576cdb0872842bec4367512b8885a52ec7b0dc
Alexey Alekhin <laughedelic@gmail.com>
2015-04-06 00:47:38 +0200

    Finished preliminary updating/simplification

--- build.sbt
+++ build.sbt
@@ -1,21 +1,15 @@
 Nice.scalaProject
 
-sbtPlugin := true
-
 name := "sbt-statika"
-
 description := "Default sbt project settings for statika bundles"
-
 organization := "ohnosequences"
 
+sbtPlugin := true
+scalaVersion := "2.10.5"
 bucketSuffix := "era7.com"
 
-
 // plugins which will be inherrited by anybody who uses this plugin:
-addSbtPlugin("ohnosequences" % "sbt-s3-resolver" % "0.7.0")
+addSbtPlugin("ohnosequences" % "sbt-s3-resolver" % "0.13.0-SNAPSHOT")
+addSbtPlugin("ohnosequences" % "nice-sbt-settings" % "0.6.0-SNAPSHOT")
 
-addSbtPlugin("ohnosequences" % "nice-sbt-settings" % "0.3.1")
-
-addSbtPlugin("com.typesafe.sbt" % "sbt-start-script" % "0.10.0")
-
-generateDocs := {}
+dependencyOverrides += "commons-codec" % "commons-codec" % "1.7"
--- src/main/scala/SbtStatika.scala
+++ src/main/scala/SbtStatika.scala
@@ -1,145 +1,115 @@
 package ohnosequences.sbt
 
-import sbt._
-import Keys._
+import sbt._, Keys._
 
-import com.typesafe.sbt.SbtStartScript._
+import ohnosequences.sbt._
+import SbtS3Resolver._, autoImport._
+import nice._, NiceProjectConfigs._, ResolverSettings._
 
-import ohnosequences.sbt.SbtS3Resolver._
-import ohnosequences.sbt.NiceSettingsPlugin._
-import ohnosequences.sbt.statika.Utils._
+object SbtStatikaPlugin extends AutoPlugin {
 
-object SbtStatikaPlugin extends sbt.Plugin {
+  object autoImport {
 
-  lazy val statikaVersion = settingKey[String]("Statika library version")
-  lazy val awsStatikaVersion = settingKey[String]("AWS-Statika library version")
-  lazy val publicResolvers = settingKey[Seq[Resolver]]("Public S3 resolvers for the bundle dependencies")
-  lazy val privateResolvers = settingKey[Seq[S3Resolver]]("Private S3 resolvers for the bundle dependencies")
-  lazy val metadataObject = settingKey[String]("Name of the generated metadata object")
-
-  //////////////////////////////////////////////////////////////////////////////
-
-  object Statika {
-
-    lazy val bundleProject: Seq[Setting[_]] = 
-      (startScriptForClassesSettings: Seq[Setting[_]]) ++ 
-      (Nice.scalaProject: Seq[Setting[_]]) ++ Seq(
-
-      // resolvers needed for statika dependency
-        resolvers ++= Seq ( 
-          "Era7 public maven releases"  at toHttp("s3://releases.era7.com")
-        , "Era7 public maven snapshots" at toHttp("s3://snapshots.era7.com")
-        ) 
-
-      , bucketSuffix := {"statika." + organization.value + ".com"}
-
-      , publicResolvers := Seq(
-            Resolver.url("Statika public ivy releases", url(toHttp("s3://releases."+bucketSuffix.value)))(ivy)
-          , Resolver.url("Statika public ivy snapshots", url(toHttp("s3://snapshots."+bucketSuffix.value)))(ivy)
-          )
-
-      , privateResolvers := {
-          if (!isPrivate.value) Seq() else Seq(
-              S3Resolver("Statika private ivy releases",  "s3://private.releases."+bucketSuffix.value, ivy)
-            , S3Resolver("Statika private ivy snapshots", "s3://private.snapshots."+bucketSuffix.value, ivy)
-            )
-        }
-
-      // adding privateResolvers to normal ones, if we have credentials
-      , resolvers ++= 
-            publicResolvers.value ++
-          { privateResolvers.value map { r => s3credentials.value map r.toSbtResolver } flatten }
-
-      // publishing (ivy-style by default)
-      , publishMavenStyle := false
-      // disable publishing sources
-      , publishArtifact in (Compile, packageSrc) := false
-
-      , statikaVersion := "1.0.0"
-      , awsStatikaVersion := "1.0.0"
-
-      // dependencies
-      , libraryDependencies ++= Seq (
-          "ohnosequences" %% "statika" % statikaVersion.value
-        , "org.scalatest" %% "scalatest" % "2.0" % "test"
-        )
-      )
-  
-
-    lazy val distributionProject: Seq[Setting[_]] = bundleProject ++ Seq(
-
-        libraryDependencies += "ohnosequences" %% "aws-statika" % awsStatikaVersion.value
-
-      // metadata generation
-      , metadataObject := name.value.split("""\W""").map(_.capitalize).mkString
-      , sourceGenerators in Compile += task[Seq[File]] {
-          // helps to serialize Strings correctly:
-          def seqToStr(rs: Seq[String]) = 
-            if  (rs.isEmpty) "Seq()"  
-            else rs.mkString("Seq(\"", "\", \"", "\")")
-
-          // TODO: move it to the sbt-s3-resolver
-          def toPublic(r: S3Resolver): Resolver = {
-            if(publishMavenStyle.value) r.name at r.url
-            else Resolver.url(r.name, url(toHttp(r.url)))(r.patterns)
-          }
-          // adding publishing resolver to the right list
-          val pubResolvers = resolvers.value ++ {
-            if(isPrivate.value) Seq() else Seq(toPublic(publishS3Resolver.value))
-          }
-          val privResolvers = privateResolvers.value ++ {
-            if(isPrivate.value) Seq(publishS3Resolver.value) else Seq()
-          }
-
-          // Patterns:
-          // mvn: "[organisation]/[module]_[scalaVersion]/[revision]/[artifact]-[revision]-[classifier].[ext]"
-          // ivy: "[organisation]/[module]_[scalaVersion]/[revision]/[type]s/[artifact]-[classifier].[ext]"
-          val fatUrl = {
-            val isMvn = publishMavenStyle.value
-            val scalaV = "_"+scalaBinaryVersion.value 
-            val module = moduleName.value + scalaV
-            val artifact = 
-              (if (isMvn) "" else "jars/") + 
-              module +
-              (if (isMvn) "-"+version.value else "") + 
-              "-fat.jar"
-
-            Seq( publishS3Resolver.value.url
-               , organization.value
-               , module
-               , version.value
-               , artifact
-               ).mkString("/")
-          }
-
-          val text = """
-            |package generated.metadata
-            |
-            |import ohnosequences.statika.aws._
-            |
-            |class $metadataObject$(
-            |  val organization     : String = "$organization$"
-            |, val artifact         : String = "$artifact$"
-            |, val version          : String = "$version$"
-            |, val resolvers        : Seq[String] = $resolvers$
-            |, val privateResolvers : Seq[String] = $privateResolvers$
-            |, val artifactUrl      : String = "$fatUrl$"
-            |) extends SbtMetadata with FatJarMetadata
-            |""".stripMargin.
-              replace("$metadataObject$", metadataObject.value).
-              replace("$organization$", organization.value).
-              replace("$artifact$", name.value.toLowerCase).
-              replace("$version$", version.value).
-              replace("$resolvers$", seqToStr(pubResolvers map resolverToString flatten)).
-              replace("$privateResolvers$", seqToStr(privResolvers map (_.toString))).
-              replace("$fatUrl$", fatUrl)
-
-          val file = (sourceManaged in Compile).value / "metadata.scala" 
-          IO.write(file, text)
-          Seq(file)
-        }
-      ) ++ Nice.fatArtifactSettings
-
+    lazy val statikaVersion = settingKey[String]("Statika library version")
+    lazy val awsStatikaVersion = settingKey[String]("AWS-Statika library version")
+    lazy val publicResolvers = settingKey[Seq[Resolver]]("Public S3 resolvers for the bundle dependencies")
+    lazy val privateResolvers = settingKey[Seq[S3Resolver]]("Private S3 resolvers for the bundle dependencies")
+    lazy val metadataObject = settingKey[String]("Name of the generated metadata object")
   }
+  import autoImport._
+
+  // This plugin will load automatically
+  override def trigger = allRequirements
+
+  // Default settings
+  override lazy val projectSettings: Seq[Setting[_]] =
+    (Nice.scalaProject: Seq[Setting[_]]) ++ Seq(
+
+    // resolvers needed for statika dependency
+    resolvers ++= Seq (
+      "Era7 public maven releases"  at ("s3://releases.era7.com"),
+      "Era7 public maven snapshots" at ("s3://snapshots.era7.com")
+    ),
+
+    bucketSuffix := "statika." + organization.value + ".com",
+
+    publicResolvers := Seq(
+      Resolver.url("Statika public ivy releases", url(s3("releases."+bucketSuffix.value).toHttps(s3region.value.toString)))(ivy),
+      Resolver.url("Statika public ivy snapshots", url(s3("snapshots."+bucketSuffix.value).toHttps(s3region.value.toString)))(ivy)
+    ),
+
+    privateResolvers := {
+      if (!isPrivate.value) Seq() else Seq(
+        s3resolver.value("Statika private ivy releases",  s3("private.releases."+bucketSuffix.value)).withIvyPatterns,
+        s3resolver.value("Statika private ivy snapshots", s3("private.snapshots."+bucketSuffix.value)).withIvyPatterns
+      )
+    },
+
+    // adding privateResolvers to normal ones, if we have credentials
+    resolvers ++=
+      publicResolvers.value ++
+      privateResolvers.value.map{ toSbtResolver(_) },
+
+    // publishing (ivy-style by default)
+    publishMavenStyle := false,
+    // disable publishing sources
+    publishArtifact in (Compile, packageSrc) := false,
+
+    statikaVersion := "2.0.0-SNAPSHOT",
+    awsStatikaVersion := "2.0.0-SNAPSHOT",
+
+    libraryDependencies += "ohnosequences" %% "statika" % statikaVersion.value,
+
+    libraryDependencies += "ohnosequences" %% "aws-statika" % awsStatikaVersion.value,
+
+    metadataObject := name.value.split("""\W""").map(_.capitalize).mkString,
+
+    sourceGenerators in Compile += task[Seq[File]] {
+
+      // Patterns:
+      // mvn: "[organisation]/[module]_[scalaVersion]/[revision]/[artifact]-[revision]-[classifier].[ext]"
+      // ivy: "[organisation]/[module]_[scalaVersion]/[revision]/[type]s/[artifact]-[classifier].[ext]"
+      val fatUrl = {
+        val isMvn = publishMavenStyle.value
+        val scalaV = "_"+scalaBinaryVersion.value
+        val module = moduleName.value + scalaV
+        val artifact =
+          (if (isMvn) "" else "jars/") +
+          module +
+          (if (isMvn) "-"+version.value else "") +
+          "-fat.jar"
+
+        Seq(
+          publishS3Resolver.value.url,
+          organization.value,
+          module,
+          version.value,
+          artifact
+        ).mkString("/")
+      }
+
+      val text = """
+        |package generated.metadata
+        |
+        |import ohnosequences.statika.aws._
+        |
+        |class $metadataObject$(
+        |  val organization: String = "$organization$",
+        |  val artifact:     String = "$artifact$",
+        |  val version:      String = "$version$",
+        |  val artifactUrl:  String = "$fatUrl$"
+        |)
+        |""".stripMargin.
+          replace("$metadataObject$", metadataObject.value).
+          replace("$organization$", organization.value).
+          replace("$artifact$", name.value.toLowerCase).
+          replace("$version$", version.value).
+          replace("$fatUrl$", fatUrl)
+
+      val file = (sourceManaged in Compile).value / "metadata.scala"
+      IO.write(file, text)
+      Seq(file)
+    }
+  ) ++ AssemblySettings.fatArtifactSettings
 
 }
