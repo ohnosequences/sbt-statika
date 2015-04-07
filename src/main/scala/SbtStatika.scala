f9a1e5d80d27dd706f4c98f19843886a7355705c
Alexey Alekhin <laughedelic@gmail.com>
2015-04-07 15:58:59 +0200

    different improvements

--- src/main/scala/SbtStatika.scala
+++ src/main/scala/SbtStatika.scala
@@ -4,7 +4,7 @@ import sbt._, Keys._
 
 import ohnosequences.sbt._
 import SbtS3Resolver._, autoImport._
-import nice._, NiceProjectConfigs._, ResolverSettings._
+import nice._, NiceProjectConfigs._, ResolverSettings._, AssemblySettings._
 
 object SbtStatikaPlugin extends AutoPlugin {
 
@@ -14,21 +14,24 @@ object SbtStatikaPlugin extends AutoPlugin {
     lazy val awsStatikaVersion = settingKey[String]("AWS-Statika library version")
     lazy val publicResolvers = settingKey[Seq[Resolver]]("Public S3 resolvers for the bundle dependencies")
     lazy val privateResolvers = settingKey[Seq[S3Resolver]]("Private S3 resolvers for the bundle dependencies")
+    lazy val fatUrl = settingKey[String]("Fat jsr artifact url")
+    lazy val generateMetadata = taskKey[Seq[File]]("Task generating metadata object")
     lazy val metadataObject = settingKey[String]("Name of the generated metadata object")
   }
   import autoImport._
 
   // This plugin will load automatically
+  override def requires = empty
   override def trigger = allRequirements
 
   // Default settings
   override lazy val projectSettings: Seq[Setting[_]] =
-    (Nice.scalaProject: Seq[Setting[_]]) ++ Seq(
+    Nice.scalaProject ++ AssemblySettings.fatArtifactSettings ++ Seq(
 
     // resolvers needed for statika dependency
     resolvers ++= Seq (
-      "Era7 public maven releases"  at ("s3://releases.era7.com"),
-      "Era7 public maven snapshots" at ("s3://snapshots.era7.com")
+      "Era7 public maven releases"  at s3("releases.era7.com").toHttps(s3region.value.toString),
+      "Era7 public maven snapshots" at s3("snapshots.era7.com").toHttps(s3region.value.toString)
     ),
 
     bucketSuffix := "statika." + organization.value + ".com",
@@ -52,8 +55,9 @@ object SbtStatikaPlugin extends AutoPlugin {
 
     // publishing (ivy-style by default)
     publishMavenStyle := false,
-    // disable publishing sources
+    // disable publishing sources and docs
     publishArtifact in (Compile, packageSrc) := false,
+    publishArtifact in (Compile, packageDoc) := false,
 
     statikaVersion := "2.0.0-SNAPSHOT",
     awsStatikaVersion := "2.0.0-SNAPSHOT",
@@ -64,52 +68,55 @@ object SbtStatikaPlugin extends AutoPlugin {
 
     metadataObject := name.value.split("""\W""").map(_.capitalize).mkString,
 
-    sourceGenerators in Compile += task[Seq[File]] {
+    // mvn: "[organisation]/[module]_[scalaVersion]/[revision]/[artifact]-[revision]-[classifier].[ext]"
+    // ivy: "[organisation]/[module]_[scalaVersion]/[revision]/[type]s/[artifact]-[classifier].[ext]"
+    fatUrl := {
+      val isMvn = publishMavenStyle.value
+      val scalaV = "_"+scalaBinaryVersion.value
+      val module = moduleName.value + scalaV
+      val artifact =
+        (if (isMvn) "" else "jars/") +
+        module +
+        (if (isMvn) "-"+version.value else "") +
+        "-"+fatArtifactClassifier.value +
+        ".jar"
 
-      // Patterns:
-      // mvn: "[organisation]/[module]_[scalaVersion]/[revision]/[artifact]-[revision]-[classifier].[ext]"
-      // ivy: "[organisation]/[module]_[scalaVersion]/[revision]/[type]s/[artifact]-[classifier].[ext]"
-      val fatUrl = {
-        val isMvn = publishMavenStyle.value
-        val scalaV = "_"+scalaBinaryVersion.value
-        val module = moduleName.value + scalaV
-        val artifact =
-          (if (isMvn) "" else "jars/") +
-          module +
-          (if (isMvn) "-"+version.value else "") +
-          "-fat.jar"
+      Seq(
+        publishS3Resolver.value.url,
+        organization.value,
+        module,
+        version.value,
+        artifact
+      ).mkString("/")
+    },
 
-        Seq(
-          publishS3Resolver.value.url,
-          organization.value,
-          module,
-          version.value,
-          artifact
-        ).mkString("/")
-      }
+    generateMetadata := {
 
       val text = """
         |package generated.metadata
         |
-        |import ohnosequences.statika.aws._
+        |import ohnosequences.statika.bundles._
         |
-        |class $metadataObject$(
-        |  val organization: String = "$organization$",
-        |  val artifact:     String = "$artifact$",
-        |  val version:      String = "$version$",
+        |case object $metadataObject$ extends AnyArtifactMetadata {
+        |  val organization: String = "$organization$"
+        |  val artifact:     String = "$artifact$"
+        |  val version:      String = "$version$"
         |  val artifactUrl:  String = "$fatUrl$"
-        |)
+        |}
         |""".stripMargin.
           replace("$metadataObject$", metadataObject.value).
           replace("$organization$", organization.value).
           replace("$artifact$", name.value.toLowerCase).
           replace("$version$", version.value).
-          replace("$fatUrl$", fatUrl)
+          replace("$fatUrl$", fatUrl.value)
 
-      val file = (sourceManaged in Compile).value / "metadata.scala"
+      val file = (sourceManaged in Compile).value / "statika" / "metadata.scala"
       IO.write(file, text)
       Seq(file)
-    }
-  ) ++ AssemblySettings.fatArtifactSettings
+    },
+
+    sourceGenerators in Compile += generateMetadata.taskValue
+
+  )
 
 }
